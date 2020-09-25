6bc76dc187e227805e6bd9a492a00aebc0c3ff94
Ozioma Uzoegwu <oziomauzoegwu@gmail.com>
2020-09-25 04:58:57 +0100

    added amplify app creation and buildspec file

--- amplify-infra/lib/amplify-infra-stack.ts
+++ amplify-infra/lib/amplify-infra-stack.ts
@@ -1,9 +1,28 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as codecommit from "@aws-cdk/aws-codecommit";
+import * as amplify from "@aws-cdk/aws-amplify";
 
 export class AmplifyInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Part 1 [Optional] - Creation of the source control repository
+    const amplifyReactSampleRepo = new codecommit.Repository(
+      this,
+      "AmplifyReactTestRepo",
+      {
+        repositoryName: "amplify-react-test-repo",
+        description:
+          "CodeCommit repository that will be used as the source repository for the sample react app and the cdk app",
+      }
+    );
+
+    // Part 2 - Creation of the Amplify Application
+    const amplifyApp = new amplify.App(this, "sample-react-app ", {
+      sourceCodeProvider: new amplify.CodeCommitSourceCodeProvider({
+        repository: amplifyReactSampleRepo,
+      }),
+    });
+    const masterBranch = amplifyApp.addBranch("master");
   }
 }
